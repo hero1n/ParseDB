23a84db4898c0a60ac653eb9ff125c42f6445b57
kopswq <heroine_n@naver.com>
2015-08-25 10:18:27 +0900

    Initial Commit

---
+++ DBTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DBTest
+//
+//  Created by Jaewon on 2015. 8. 25..
+//  Copyright (c) 2015년 App:ple Pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
